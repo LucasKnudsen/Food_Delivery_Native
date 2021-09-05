10422decf62ca13278dd8c2af1cbe40990c6f7f5
Lucas <laksteelhouse@gmail.com>
2021-09-05 16:55:06 +0200

    Styles tabs

--- App.js
+++ App.js
@@ -9,12 +9,12 @@ const Stack = createStackNavigator()
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{
+      <Stack.Navigator 
+      screenOptions={{
         headerShown: false
       }}
-      initialRouteName={'Home'}
      >
-        <Stack.Screen name='Home' component={Tabs} />
+        <Stack.Screen name='Main' component={Tabs} />
         <Stack.Screen name='Restaurant' component={Restaurant} />
         <Stack.Screen name='OrderDelivery' component={OrderDelivery} />
       </Stack.Navigator>
--- navigation/Tabs.jsx
+++ navigation/Tabs.jsx
@@ -19,7 +19,7 @@ const TabBarCustomButton = ({accessibilityState, children, onPress}) => {
                     <Svg
                         width={70}
                         height={61}
-                        viewBox="0 0 75 61"
+                        viewBox="0 0 75 66"
                     >
                         <Path
                             d="M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
@@ -56,16 +56,22 @@ const TabBarCustomButton = ({accessibilityState, children, onPress}) => {
   }
 }
 
+
+
 const Tabs = () => {
   return (
    <Tab.Navigator
-    screenOptions={{tabBarShowLabel: false,
-    tabBarStyle: {
-      backgroundColor: 'transparent',
-      marginBottom: 10,
-      borderTopWidth: 0,
-      elevation: 0,
-    }}}
+    screenOptions={{
+      headerShown: false,
+      tabBarShowLabel: false,
+      tabBarStyle: {
+        backgroundColor: 'transparent',
+        marginBottom: 10,
+        borderTopWidth: 0,
+        elevation: 0,
+      }
+    }}
+
    >
      <Tab.Screen 
       name='Home'
